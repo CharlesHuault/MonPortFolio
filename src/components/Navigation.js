c8b7e12aa4fe80979b0ef14a574c42f2793fcd0e
CharlesHuault <chuault@gmail.com>
2023-08-31 18:32:50 +0200

    Création Header, Composant Navigation et premier composant de lien

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Lienpresentation from './Lienpresentation';
+
+const Navigation = () => {
+    return (
+        <div>
+            <Lienpresentation />
+            
+        </div>
+    );
+};
+
+export default Navigation;
