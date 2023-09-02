2d4d724ea8f39a3a05441d490f2a83bd2b306617
CharlesHuault <chuault@gmail.com>
2023-09-02 23:59:48 +0200

    Intégration du contenu des cartes et leur style

--- src/components/Elementprojets.js
+++ src/components/Elementprojets.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Card from './Card';
+import '../styles/components/elementprojets.scss'
 
 const Elementprojets = () => {
     return (
         <div className='cards-list'>
             <Card cover='./MaqDeskBooki.png' />
-            <Card cover='./MaqDeskSophie/png'/>
+            <Card cover='./MaqDeskSophie.png' />
+            <Card cover='./MadDeskMenu.png' />
+            <Card cover='./MaqDeskNina.png' />
+            <Card cover='./MaqDeskKasa.png' />
+            <Card cover='./MaqDeskGrim.png' />
         </div>
     );
 };
