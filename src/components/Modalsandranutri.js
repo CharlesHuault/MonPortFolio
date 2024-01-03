86f8b235c894a01dd78dd5679de6416c098a5f80
CharlesHuault <chuault@gmail.com>
2024-01-03 19:20:23 +0100

    Retrait du lien github de la modale sandra

--- src/components/Modalsandranutri.js
+++ src/components/Modalsandranutri.js
@@ -48,7 +48,6 @@ const Modalchwebvision = ({ isOpen, closeModal }) => {
         <div className='modal-body'>         
             <div className='liensand-container'>
               <a href="https://www.sandralagouarde.com/" target="_blank" rel="noopener noreferrer"><button className='liensand'>{t('sandmodal.link')}</button></a>
-              <a href="https://github.com/CharlesHuault/Sandra-nutricoach" target="_blank" rel="noopener noreferrer"><button className='liensand'>{t('sandmodal.link2')}</button></a>
             </div>
       </div>
     </ReactModal>
